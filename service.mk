5eb1ebcd87a8bc6b34ac602890375b76bc20b4b6
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-05-09 23:08:58 +0200

    Improve docker usage and migrations

--- service.mk
+++ service.mk
@@ -5,25 +5,25 @@ include proto.mk
 DB_USER ?= root
 DB_PASS ?=
 DB_HOST ?= 127.0.0.1
-DB_PORT ?= 3336
+DB_PORT ?= 3306
 DB_NAME ?= service
 
-.PHONY: start migrate
+.PHONY: migrate
+
+migrate: ## Run migrations
+ifdef GODOTENV
+	@${GODOTENV} $(MAKE) GODOTENV="" migrate
+else
+	migrate -path ${PWD}/migrations/ -database mysql://${DB_USER}:${DB_PASS}@tcp\(${DB_HOST}:${DB_PORT}\)/${DB_NAME} up
+endif
 
 docker-compose.override.yml: ## Create docker-compose override file
 	cp docker-compose.override.yml.example docker-compose.override.yml
 
-start: docker-compose.override.yml ## Start docker env
+setup::
 	mkdir -p var/
-	docker-compose up -d
-
-migrate: ## Run migrations
-	migrate -path ${PWD}/migrations/ -database mysql://${DB_USER}:${DB_PASS}@tcp\(${DB_HOST}:${DB_PORT}\)/${DB_NAME} up
-
-setup:: docker-compose.override.yml
 
 clean::
-	docker-compose down
 	rm -rf var/ docker-compose.override.yml
 
 envcheck::
