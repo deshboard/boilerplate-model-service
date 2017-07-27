da67532d1662f1656e8cd76ab76821587b211f51
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-07-27 23:01:49 +0200

    Remove test configuration

--- app/txdb_test.go
+++ app/txdb_test.go
@@ -7,33 +7,20 @@ import (
 
 	txdb "github.com/DATA-DOG/go-txdb"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/kelseyhightower/envconfig"
+	"github.com/goph/stdlib/os"
 )
 
 func init() {
-	config := &testConfiguration{}
-
-	envconfig.MustProcess("", config)
-
 	txdb.Register(
 		"txdb",
 		"mysql",
 		fmt.Sprintf(
 			"%s:%s@tcp(%s:%d)/%s?parseTime=true",
-			config.DbUser,
-			config.DbPass,
-			config.DbHost,
-			config.DbPort,
-			config.DbName,
+			os.MustEnv("DB_USER"),
+			os.MustEnv("DB_PASS"),
+			os.MustEnv("DB_HOST"),
+			os.MustEnv("DB_PORT"),
+			os.MustEnv("DB_NAME"),
 		),
 	)
-}
-
-// testConfiguration is a subset of main.configuration necessary for testing.
-type testConfiguration struct {
-	DbHost string `split_words:"true" required:"true"`
-	DbPort int    `split_words:"true" default:"3306"`
-	DbUser string `split_words:"true" required:"true"`
-	DbPass string `split_words:"true" required:"true"`
-	DbName string `split_words:"true" required:"true"`
 }
