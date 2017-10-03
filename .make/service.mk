9c5b8459422fdf1a8fa532718db9442b5c3abae8
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-10-03 18:24:54 +0200

    Remove docker make stuff

--- .make/service.mk
+++ .make/service.mk
@@ -9,16 +9,9 @@ DB_PORT ?= 3306
 DB_NAME ?= service
 
 .PHONY: migrate
-
 migrate: ## Run migrations
 ifdef GODOTENV
 	@${GODOTENV} $(MAKE) GODOTENV="" migrate
 else
 	migrate -path ${PWD}/migrations/ -database mysql://${DB_USER}:${DB_PASS}@tcp\(${DB_HOST}:${DB_PORT}\)/${DB_NAME} up
 endif
-
-setup::
-	mkdir -p .docker/
-
-clean::
-	rm -rf .docker/ docker-compose.override.yml
